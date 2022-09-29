101571348911d354544e9efd3e88dea2996f5cba
IsmoiljonJabborov <190107106@stu.sdu.edu.kz>
2022-09-29 10:08:58 +0500

    I did not get the task

--- src/main/java/lang/print/gaps/task3/ReassigningValues.java
+++ src/main/java/lang/print/gaps/task3/ReassigningValues.java
@@ -3,15 +3,17 @@ package lang.print.gaps.task3;
 public class ReassigningValues {
     public static void main(String[] args) {
         //write code here
-        int first = 1;
-        int second = 10;
-        int third = 100;
+        int first, second, third;
+         first = 1;
+         second = 10;
+         third = 100;
 
         System.out.println(first + "\n" + second + "\n" + third);
 
-        int linkToFirst = first;
-        int linkToSecond = second;
-        int linkToThird = third;
+        int linkToFirst, linkToSecond, linkToThird;
+         linkToFirst = first;
+         linkToSecond = second;
+         linkToThird = third;
 
         first = 15;
         second = 6;
